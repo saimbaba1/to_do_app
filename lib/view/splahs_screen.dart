00c3c9575355a291824cbc35353b1bfd0f214ca4
Saim Baba <uzair@Saim-Baba-MacBook-Air.local>
2025-02-06 12:56:16 +0500

    message

---
+++ lib/view/splahs_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/view/auth/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.home),
+            const SizedBox(height: 20),
+            const SizedBox(height: 30),
+            CircularProgressIndicator(color: Colors.black),
+          ],
+        ),
+      ),
+    );
+  }
+}
